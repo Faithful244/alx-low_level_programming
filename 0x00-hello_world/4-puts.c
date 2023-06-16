ff7937bc118967b26f109ed5294a9d671be6ffe4
faithful244 <chijinduamos@gmail.com>
2023-06-16 07:45:54 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - prints "Programming is like building a multilingual puzzle,
- * followed by a new line."
+ * followed by a new line.
  *
- * Return Always 0 (success)
+ * Return : Always 0 (success)
  */
 int main(void)
 {
