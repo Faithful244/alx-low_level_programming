6bdbcc85846dfe05d6b5112abc006212de851658
faithful244 <chijinduamos@gmail.com>
2023-06-16 07:53:53 -0700

    alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
+
 /**
  * main - prints "Programming is like building a multilingual puzzle,
  * followed by a new line.
  *
  * Return : Always 0 (success)
  */
+
 int main(void)
+
 {
 	puts( "\"Programming is like building a multilingual puzzle");
 
