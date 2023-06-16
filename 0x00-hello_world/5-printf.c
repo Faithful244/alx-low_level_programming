b4a06b5c079442659f48bd8e1802300da7fe8ad5
faithful244 <chijinduamos@gmail.com>
2023-06-16 08:26:31 -0700

    alx

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art,"
- *followed by new line
+ * main - prints "with proper grammar
+ * , but the outcome is a piece of art,"followed by new line
  * Return: Always 0 (Success)
  */
 
